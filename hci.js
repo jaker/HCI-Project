b080c141c8690823eeca6aa5caa21cf27e666208
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2009-11-23 05:09:37 +0800

    Added form that is itnercepted by javascript and adds the results to the page.
    I also added a 'hci' namespace that holds all the helper functions.

--- hci.js
+++ hci.js
@@ -1,24 +1,34 @@
+var hci = {};
+
 $(document).ready(function(){
-    searchPipes("heart+disease");
+  $("form").attr("action","javascript:hci.getInputToSearch()");
 });
 
-function searchPipes(keywords) {
+hci.getInputToSearch = function() {
+  var keywords = $("input:first").val();
+  if (keywords !== '') {
+    hci.searchPipes(keywords);
+  }
+};
+
+hci.searchPipes = function(keywords) {
  var request = "http://pipes.yahoo.com/pipes/pipe.run?_id=654155481dd4460e6353d5f46ad1dd73&_render=json&_callback=?&searchTerm=" + keywords;
  $.getJSON(request, function(json){
-     if(json.count > 0) {
-       displayJSON(json);
-     } else {
-       $("#results").html("Search not found.");
-     }}
-  );
-}
+   if(json.count > 0) {
+     hci.displayJSON(json);
+   } else {
+     $("#results").html("Search not found.");
+   }}
+ );
+};
 
-function displayJSON(data) {
-    data
-    $.each(data.value.items, function(i,item){
-        $("#results").append("<h1>"+item.title+"</h1>");
-        $("#results").append("<h1>"+item.link+"</h1>");
-        $("#results").append("<p>"+item.description+"</p>");
-    });
+hci.displayJSON = function(data) {
+  $.each(data.value.items, function(i,item){
+    var aResult = $(document.createElement("div")).addClass("result");
+    var aTitle = $(document.createElement("h1")).html(item.title);
+    var aDescription = $(document.createElement("p")).html(item.description);
+    aResult.append(aTitle).append(aDescription);
+    $("#results").append(aResult);
+  });
+};
 
-}
